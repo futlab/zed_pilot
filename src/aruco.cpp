0f9d1a2559c97a0b4bbee6397e1fc30bd841ee23
igor smirnov <smirnoviv@rambler.ru>
2018-09-05 18:58:30 +0500

    Developing Aruco optimizers

--- src/aruco.cpp
+++ src/aruco.cpp
@@ -74,16 +74,15 @@ private:
     std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
     std::map<int, Marker> markers;
     std::vector<Frame> frames;
+    map<int, int> markerVars;
     cv::Mat gray;
     const Aruco *const parent;
     SE3d cameraPose;
-    double markerDefSize;
 public:
     Aruco_(const Aruco *parent) :
         dictionary(cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250)),
         parameters(cv::aruco::DetectorParameters::create()),
-        parent(parent),
-        markerDefSize(0.05)
+        parent(parent)
     {
         parameters->cornerRefinementMethod = cv::aruco::CORNER_REFINE_CONTOUR;//CORNER_REFINE_SUBPIX;
         parameters->cornerRefinementMinAccuracy = 0.05;
@@ -106,8 +105,10 @@ public:
             initializeByFrame(*frame);
         else {
             frame->pose = cameraPose;
-			optimize();
-		}
+            //optimize<true, false>();
+            optimizeFrameTranslation(*frame);
+            optimizeFramePose(*frame);
+        }
         cameraPose = frame->pose;
 #ifdef WITH_GUI
 		cv::Mat leftRGB, rightRGB;
@@ -147,10 +148,7 @@ public:
 	}
 private:
 	template<bool right>
-	Vector2d inline projectPoint(const Vector3d &point)
-	{
-		return Vector2d(point.x() + parent->baseline * (right ? -0.5 : 0.5), point.y()) / point.z();
-	}
+    Vector2d inline projectPoint(const Vector3d &point) { return Vector2d(point.x() + parent->baseline * (right ? -0.5 : 0.5), point.y()) / point.z(); }
     void imageToMarkers(const cv::Mat &image, std::vector<int> &markerIds, std::vector<std::vector<cv::Point2f>> &markerCorners/*, std::vector<cv::Vec3d> &rvecs, std::vector<cv::Vec3d> &tvecs*/)
     {
         using namespace cv;
@@ -205,46 +203,43 @@ private:
 		rm = frame.pose.rotationMatrix();
 		Vector3d translation = frame.pose.translation();
         for (auto p : frame.markers) {
-            auto ir = markers.insert(make_pair(p.first, Marker(p.second, rm, translation, parent->baseline)));
+            /*auto ir = */markers.insert(make_pair(p.first, Marker(p.second, rm, translation, parent->baseline)));
 			//if (!ir.second)
 			//	ir.first->second = p.second;
         }
     }
-	template<bool right, typename Derived>
-	inline void dUV_dPose(
-		const Vector3d& framePoint,
-		Block<Derived, 2, 6> &J)
+    template<bool right, bool withT, bool withR, typename Derived>
+    inline void dUV_dPose(const Vector3d& framePoint, DenseBase<Derived> &J)
 	{
-		const double x = framePoint[0] + parent->baseline * (right ? -0.5 : 0.5);
-		const double y = framePoint[1];
-		const double z = framePoint[2];
-		const double z_inv = 1 / z;
-		const double z_inv_2 = z_inv * z_inv;
-
-		J(0, 0) = z_inv;              // -1/z
-		J(0, 1) = 0.0;                 // 0
-		J(0, 2) = -x * z_inv_2;           // x/z^2
-		J(0, 3) = -x * y * z_inv_2;            // x*y/z^2
-		J(0, 4) = 1.0 - x * J(0, 2);   // -(1.0 + x^2/z^2)
-		J(0, 5) = -y * z_inv;             // y/z
-		
-		J(1, 0) = 0.0;                 // 0
-		J(1, 1) = z_inv;              // -1/z
-		J(1, 2) = -y * z_inv_2;           // y/z^2
-		J(1, 3) = -1.0 + y * J(1, 2);      // 1.0 + y^2/z^2
-		J(1, 4) = J(0, 3);             // -x*y/z^2
-		J(1, 5) = -x * z_inv;            // x/z
+        long row = 0, col = 0;
+        const double x = framePoint[0] + parent->baseline * (right ? -0.5 : 0.5), y = framePoint[1], z = framePoint[2],
+            z_inv = 1 / z, z_inv_2 = z_inv * z_inv;
+        static_assert (withT, "withT required");
+        if (withT) {
+            J(row + 0, col + 0) = z_inv;                // -1/z
+            J(row + 0, col + 1) = 0.0;                  // 0
+            J(row + 0, col + 2) = -x * z_inv_2;         // x/z^2
+            J(row + 1, col + 0) = 0.0;                  // 0
+            J(row + 1, col + 1) = z_inv;                // -1/z
+            J(row + 1, col + 2) = -y * z_inv_2;         // y/z^2
+            col += 3;
+        }
+        if (withR) {
+            J(row + 0, col + 0) = -x * y * z_inv_2;     // x*y/z^2
+            J(row + 0, col + 1) = 1.0 - x * J(row + 0, col + 2);    // -(1.0 + x^2/z^2)
+            J(row + 0, col + 2) = -y * z_inv;           // y/z
+            J(row + 1, col + 0) = -1.0 + y * J(row + 1, 2);   // 1.0 + y^2/z^2
+            J(row + 1, col + 1) = J(row + 0, col + 0);        // -x*y/z^2
+            J(row + 1, col + 2) = -x * z_inv;           // x/z
+        }
 	}
-	template<bool right, typename Derived>
+    template<bool right, typename Derived>
 	inline void dUV_dPoint(
 		const Vector3d& framePoint,
 		const Matrix3d& rotation,
-		Block<Derived, 2, 3> &J)
+        DenseBase<Derived> &J)
 	{
-		const double x = framePoint[0] + parent->baseline * (right ? -0.5 : 0.5);
-		const double y = framePoint[1];
-		const double z = framePoint[2];
-
+        const double x = framePoint[0] + parent->baseline * (right ? -0.5 : 0.5), y = framePoint[1], z = framePoint[2];
 		J = (Vector2d(x, y) * rotation.block<1, 3>(2, 0) - rotation.block<2, 3>(0, 0) * z) / (z * z);
 	}
 	void correct(VectorXd &dT, const map<int, int> &markerVars)
@@ -283,7 +278,8 @@ private:
 			for (int i = 0; i < 4; ++i)
 				m.second.points[i] = m.second.prevPoints[i];
 	}
-	void testJ(MatrixXd &J, const VectorXd &e, const map<int, int> &markerVars)
+    template<bool withT, bool withR = false>
+    void testJ(MatrixXd &J, const VectorXd &e)
 	{
 		const double eps = 1E-6;
 		VectorXd e1;
@@ -296,18 +292,15 @@ private:
 		Vector6d dT = Vector6d::Zero();
 		for (auto &f : frames) {
 			auto t = f.pose;
-			auto toFrame0 = f.pose.inverse();
-			auto r0 = toFrame0.rotationMatrix();
-			auto t0 = toFrame0.translation();
-			//auto framePoint = r0 * wp + t0;
-			for (size_t i = 0; i < 6; i++) {
+            long bi = withT ? 0 : 3, ei = withR ? 6 : 3;
+            for (long i = bi; i < ei; ++i) {
 				dT[i] = eps;
 				//auto tr = f.pose.translation();
 
 				f.pose *= SE3d::exp(dT);
 
 				dT[i] = 0;
-				calcJEpoints<false>(J1, e1, markerVars);
+                calcJE<true, false>(J1, e1);
 				Jest = (e1 - e) / eps;
 				for (int row = 0; row < Jest.rows(); ++row) {
 					double j = J(row, frameCol + i);
@@ -320,24 +313,25 @@ private:
 				f.pose = t;
 			}
 
-			frameCol += 6;
-			//f.pose = SE3d::exp(dT.segment<6>(col)) * f.pose;
+            if (withR || withT)
+                frameCol += (withT && withR) ? 6 : 3;
+            //f.pose = SE3d::exp(dT.segment<6>(col)) * f.pose;
 			for (auto &m : f.markers) {
 				auto *worldPoints = markers.at(m.first).points;
 				int col = markerVars.find(m.first)->second;
 
 				for (size_t i = 0; i < 4; ++i) {
-					for (size_t d = 0; d < 3; ++d) {
+                    for (long d = 0; d < 3; ++d) {
 						double t = worldPoints[i][d];
 						worldPoints[i][d] += eps;
-						calcJEpoints<false>(J1, e1, markerVars);
+                        calcJE<true, false>(J1, e1);
 						worldPoints[i][d] = t;
 						Jest = (e1 - e) / eps;
 						for (int row = 0; row < Jest.rows(); ++row) {
 							double j = J(row, col + d);
 							double jest = Jest(row);
 							if (abs(j - jest) > 1E-5) {
-								//cout << "wrong j";
+                                cout << "wrong j";
 							}
 						}
 					}
@@ -345,10 +339,10 @@ private:
 				}
 			}
 		}
-	}
+    }
 
-	template<bool withPose>
-	void calcJEpoints(MatrixXd &J, VectorXd &e, const map<int, int> &markerVars)
+    template<bool withT, bool withR = false>
+    void calcJE(MatrixXd &J, VectorXd &e)
 	{
 		assert(J.rows() == e.rows());
 		int row = 0, frameCol = 0;
@@ -356,106 +350,193 @@ private:
 			auto toFrame = f.pose.inverse();
 			auto rotation = toFrame.rotationMatrix();
 			auto translation = toFrame.translation();
-			auto frameToWorld = f.pose.rotationMatrix();
 			for (auto &m : f.markers) {
 				const auto *worldPoints = markers.at(m.first).points;
 				int col = markerVars.find(m.first)->second;
 				for (size_t i = 0; i < 4; ++i) {
 					auto framePoint = rotation * worldPoints[i] + translation;
 					e.segment<2>(row) = m.second.leftPoints[i] - projectPoint<false>(framePoint);
-					if (withPose)
-						dUV_dPose<false>(framePoint, J.template block<2, 6>(row, frameCol));
-					dUV_dPoint<false>(framePoint, rotation, J.template block<2, 3>(row, col));
+                    if (withR || withT) {
+                        auto j = J.template block<2, (withT && withR) ? 6 : 3>(row, frameCol);
+                        dUV_dPose<false, withT, withR>(framePoint, j);
+                    }
+                    {
+                        assert(col + 3 <= J.cols());
+                        auto j = J.template block<2, 3>(row, col);
+                        dUV_dPoint<false>(framePoint, rotation, j);
+                    }
 					row += 2;
 					e.segment<2>(row) = m.second.rightPoints[i] - projectPoint<true>(framePoint);
-					if (withPose)
-						dUV_dPose<true>(framePoint, J.template block<2, 6>(row, frameCol));
-					dUV_dPoint<true>(framePoint, rotation, J.template block<2, 3>(row, col));
+                    if (withR || withT) {
+                        auto j = J.template block<2, (withT && withR) ? 6 : 3>(row, frameCol);
+                        dUV_dPose<true, withT, withR>(framePoint, j);
+                    }
+                    {
+                        auto j = J.template block<2, 3>(row, col);
+                        dUV_dPoint<true>(framePoint, rotation, j);
+                    }
 					row += 2;
 					col += 3;
 				}
 			}
-			if (withPose)
-				frameCol += 6;
+            if (withR || withT)
+                frameCol += (withT && withR) ? 6 : 3;
 		}
 		assert(row == J.rows());
-	}
-	void calcJEposes(MatrixXd &J, VectorXd &e)
-	{
-		assert(J.rows() == e.rows());
-		int row = 0, frameCol = 0;
-		for (auto &f : frames) {
-			auto toFrame = f.pose.inverse();
-			auto rotation = toFrame.rotationMatrix();
-			auto translation = toFrame.translation();
-			auto frameToWorld = f.pose.rotationMatrix();
-			for (auto &m : f.markers) {
-				const auto *worldPoints = markers.at(m.first).points;
-				for (size_t i = 0; i < 4; ++i) {
-					auto framePoint = rotation * worldPoints[i] + translation;
-					e.segment<2>(row) = m.second.leftPoints[i] - projectPoint<false>(framePoint);
-					dUV_dPose<false>(framePoint, J.template block<2, 6>(row, frameCol));
-					row += 2;
-					e.segment<2>(row) = m.second.rightPoints[i] - projectPoint<true>(framePoint);
-					dUV_dPose<true>(framePoint, J.template block<2, 6>(row, frameCol));
-					row += 2;
-				}
-			}
-				frameCol += 6;
-		}
-		assert(row == J.rows());
-	}
-	bool optimize()
+    }
+
+    template<bool withT, bool withR = false>
+    bool createJE(MatrixXd &J, VectorXd &e)
     {
-		size_t 
-			varCount = 0, 
-			eqCount = 0;
+        long
+            varCount = long(frames.size()) * ((withR ? 3 : 0) + (withT ? 3 : 0)),
+            eqCount = 0;
 
-		map<int, int> markerVars;
-		for (const auto &f : frames) {
-			for (const auto &m : f.markers) {
-				auto inserted = markerVars.insert(make_pair(m.first, int(varCount)));
-				if (inserted.second) {
-					varCount += 12; // 4 points with 3 DoF
-				}
-				eqCount += 16; // 4 points with 2 coords on 2 images
-			}
-		}
-		if (!eqCount)
-			return false;
-		
-		MatrixXd Jpoints, Jpose;
-		VectorXd e;
-		Jpoints.resize(eqCount, varCount);
-		Jpoints.setZero();
-		Jpose.resize(eqCount, 6 * frames.size());
-		Jpose.setZero();
-		e.resize(eqCount);
+        markerVars.clear();
 
-		//std::cout << "calcErrors:" << std::endl;
-		double ebest = 0;
-		for (int it = 1; it < 10; it++) {
-			calcJEposes(Jpose, e);
-			//testJ(J, e, markerVars);
+        for (const auto &f : frames) {
+            for (const auto &m : f.markers) {
+                auto inserted = markerVars.insert(make_pair(m.first, int(varCount)));
+                if (inserted.second) {
+                    varCount += 12; // 4 points with 3 DoF
+                }
+                eqCount += 16; // 4 points with 2 coords on 2 images
+            }
+        }
+        if (!eqCount)
+            return false;
+
+        J.resize(eqCount, varCount);
+        J.setZero();
+        e.resize(eqCount);
+        return true;
+    }
+
+    template<bool withT, bool withR = false>
+    bool optimize()
+    {
+        MatrixXd J;
+        VectorXd e;
+        if (!createJE<withT, withR>(J, e)) return false;
+
+        double ebest = -1;
+        for (int it = 1; it < 10; ++it) {
+            calcJE<withT, withR>(J, e);
+            testJ<withT, withR>(J, e);
 			double en = e.norm();
 			cout << "Iteration " << it << " error: " << en << endl;
-			if (ebest && (ebest <= en)) {
+            if (ebest >= 0 && ebest <= en) {
 				restore();
 				break;
 			}
 			else ebest = en;
 
-			auto a = Jpose.transpose() * Jpose;
-			auto b = Jpose.transpose() * -e;
+            auto a = J.transpose() * J;
+            auto b = J.transpose() * -e;
 			VectorXd dT = a.ldlt().solve(b);
 			correctPoses(dT);
 		}
  		return true;
     }
-	bool optimizeFrame()
-	{
+    template<bool right>
+    inline double point2ab(Matrix6d &A, Vector6d &b, const Vector2d &viewPoint, const Vector3d &framePoint)
+    {
+        Eigen::Matrix<double, 2, 6> J;
+        Vector2d e = viewPoint - projectPoint<right>(framePoint);
+        dUV_dPose<false, true, true>(framePoint, J);
 
+        A.noalias() += J.transpose() * J;
+        b.noalias() -= J.transpose() * e;
+        return e.squaredNorm();
+    }
+    //template<bool withT>
+    bool optimizeFramePose(Frame &f)
+	{
+        Matrix6d A;
+        Vector6d b;
+        double bestE = -1;
+        SE3d bestPose;
+        for (int it = 1; it < 100; ++it) {
+            A.setZero();
+            b.setZero();
+            double e = 0;
+            auto toFrame = f.pose.inverse();
+            auto rotation = toFrame.rotationMatrix();
+            auto translation = toFrame.translation();
+            for (auto &m : f.markers) {
+                const auto &fm = m.second;
+                const auto mi = markers.find(m.first);
+                if (mi == markers.end())
+                    continue;
+                const auto *worldPoints = mi->second.points;
+                for (size_t i = 0; i < 4; ++i) {
+                    auto framePoint = rotation * worldPoints[i] + translation;
+                    e += point2ab<false>(A, b, fm.leftPoints[i], framePoint);
+                    e += point2ab<true>(A, b, fm.rightPoints[i], framePoint);
+                }
+            }
+            cout << "Pose iteration " << it << " error " << e << endl;
+            if (bestE >= 0 && bestE < e) {
+                f.pose = bestPose;
+                break;
+            } else bestE = e;
+
+            Vector6d dT(A.ldlt().solve(b));
+            dT[0] = 0;
+            dT[1] = 0;
+            dT[2] = 0;
+            bestPose = f.pose;
+            f.pose *= SE3d::exp(dT * 0.2);
+        }
+        return true;
 	}
+    void optimizeFrameTranslation(Frame &f)
+    {
+        Vector3d pos = f.pose.translation(), bestPos;
+        vector<double> lens2;
+        vector<Vector3d> points;
+        for (const auto &m : f.markers) {
+            const auto mi = markers.find(m.first);
+            if (mi == markers.end())
+                continue;
+            Vector3d fp[4];
+            Marker::convertPoints(m.second.leftPoints, m.second.rightPoints, fp, parent->baseline);
+            for (size_t i = 0; i < 4; ++i) {
+                points.push_back(mi->second.points[i]);
+                lens2.push_back(fp[i].squaredNorm());
+            }
+        }
+
+        double eBest = -1;
+        for (int it = 1; it < 10; ++it) {
+            Vector3d b;
+            Matrix3d a;
+            a.setZero();
+            b.setZero();
+            double eSum = 0;
+
+            for (long i = 0; i < long(points.size()); ++i) {
+                Vector3d d = pos - points[size_t(i)];
+                const double e = d.squaredNorm() - lens2[size_t(i)];
+                b.noalias() += d * e;
+                a.noalias() += 4.0 * d * d.transpose();
+                eSum += e * e;
+            }
+            eSum = sqrt(eSum / points.size());
+            cout << "Pos iteration " << it << " error " << eSum << endl;
+            if (eBest >= 0 && eBest <= eSum) {
+                pos = bestPos;
+                break;
+            } else eBest = eSum;
+
+
+            Vector3d dT(a.ldlt().solve(b));
+            bestPos = pos;
+            pos -= dT;
+        }
+        f.pose.translation() = pos;
+        return;
+    }
 };
 
 
